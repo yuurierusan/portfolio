0096ac9daac29c87f5bed938e5d407a07cf7ebd2
Jonshea Nutson <yuurieru@icloud.com>
2023-05-01 13:07:13 -0400

    updated type issues for deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,11 +7,13 @@ import Landing from '../components/landing'
 import Nav from '../components/nav'
 import Projects from '../components/project'
 import SocialLinks from '../components/social'
+import { Analytics } from '@vercel/analytics/react'
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
     return (
         <div>
             <Component {...pageProps} />
+            <Analytics />
             <Nav />
             <div>
                 <Landing />
@@ -34,3 +36,4 @@ export default function App({ Component, pageProps }: AppProps) {
         </div>
     )
 }
+export default App
